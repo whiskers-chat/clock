2bfe69b7145e1fb82dc63f135f57694bef86717c
Finley <161162126+Finley224@users.noreply.github.com>
2024-09-24 00:07:36 -0400

    Error Handling & A new help system

--- main.ts
+++ main.ts
@@ -9,6 +9,73 @@ const meower = await meow.client.login({
   username: Deno.args[0],
   password: Deno.args[1],
 });
+
+type helpEntry = {
+  command: string[];
+  args: string;
+  desc: string;
+}
+
+let helpSections: helpEntry[] = [];
+const errorMsg = "Oh no, something went wrong. Contact support@whiskers.chat with this message and a time stamp so we can look into it."
+helpSections.push({
+  command: ["ping"],
+  args: "",
+  desc: "Check if the bot is online",
+})
+
+helpSections.push({
+  command: ["help"],
+  args: "?Page: Number",
+  desc: "Get help with the bot",
+})
+
+helpSections.push({
+  command: ["search"],
+  args: "query: string",
+  desc: "Searches for timezones from the db",
+})
+
+helpSections.push({
+  command: ["search"],
+  args: "query: string",
+  desc: "Searches for timezones from the db",
+})
+
+helpSections.push({
+  command: ["getTime", "time"],
+  args: "?TimeZone: String",
+  desc: "Get the time in a timezone, default timezone is GMT.",
+})
+
+helpSections.push({
+  command: ["getTime24", "time24"],
+  args: "?TimeZone: String",
+  desc: "Get the time in a timezone in 24H format, default timezone is GMT.",
+})
+
+function createHelpPageTables(helpSections: helpEntry[]): string[] {
+  const header = "| Command | Args | Desc |";
+  const divider = "| ------ | ------- | ------- |";
+  const base = "\n" + header + "\n" + divider + "\n";
+  let index = 0;
+  let page = 0;
+  let results: string[] = [];
+  helpSections.forEach(helpSection => {
+    if (index % 5 == 0) {
+      page++;
+      results[page - 1] = base;
+    }
+
+    results[page - 1] += `| ${helpSection.command} | ${helpSection.args} | ${helpSection.desc} |\n`
+    index++;
+  });
+
+  return results;
+}
+
+const helpPageTable = createHelpPageTables(helpSections);
+
 function getDaySection(hour: number): string {
   if (hour <= 12) {
     return "AM";
@@ -39,12 +106,6 @@ async function updateQuote(api: meow.client, newQuote: String) {
   });
 }
 
-function getParmas(command: String): RegExpMatchArray | null {
-  const regex = /(\S+)/g;
-  let parmas = command.match(regex);
-  return parmas;
-}
-
 function createTimeZoneList(): String[] {
   let timeZoneList: String[] = [];
   timezone.forEach((element) => {
@@ -64,119 +125,165 @@ meower.socket.on("create_message", (post) => {
   if (command[0].toLowerCase() != "@clock") return;
   switch (command[1].toLowerCase()) {
     case "ping": {
-      post.reply({
-        content: `@${post.username} Pong!`,
-      });
-      break;
+      try {
+        post.reply({
+          content: `@${post.username} Pong!`,
+        });
+      } catch (err) {
+        post.reply({
+          reply_to: [post.id],
+          content: errorMsg,
+        });
+      } finally {
+        break;
+      }
     }
     case "help": {
-      post.reply({
-        content: `
-          **Help Page 1:**\n
-| Command | Args | Desc |
-| -------- | ------- | ------- |
-| ping || Check if the bot is online |
-| help | ?Page: Number | This command |
-| listTimeZones || List all available timezones. |
-| getTime | ?TimeZone: String | Get the time in a timezone, default timezone is GMT. |
-| getTime24H | ?TimeZone: String | Get the time in a timezone in 24H format, default timezone is GMT. |
-          `,
-      });
-      break;
+      try {
+        let page = 1;
+        if (typeof (command[2]) != 'undefined') page = command[2];
+        post.reply({
+          content: `
+          **Help Page ${Number(page)}/${helpPageTable.length}:** ${helpPageTable[page - 1]}`,
+        });
+      } catch (err) {
+        post.reply({
+          reply_to: [post.id],
+          content: errorMsg,
+        });
+      } finally {
+        break;
+      }
     }
     case "listtimezones": {
-      const timeZoneList = [
-        "Etc/GMT",
-        "Etc/GMT+1",
-        "Etc/GMT+10",
-        "Etc/GMT+11",
-        "Etc/GMT+12",
-        "Etc/GMT+2",
-        "Etc/GMT+3",
-        "Etc/GMT+4",
-        "Etc/GMT+5",
-        "Etc/GMT+6",
-        "Etc/GMT+7",
-        "Etc/GMT+8",
-        "Etc/GMT+9",
-        "Etc/GMT-1",
-        "Etc/GMT-10",
-        "Etc/GMT-11",
-        "Etc/GMT-12",
-        "Etc/GMT-13",
-        "Etc/GMT-14",
-        "Etc/GMT-2",
-        "Etc/GMT-3",
-        "Etc/GMT-4",
-        "Etc/GMT-5",
-        "Etc/GMT-6",
-        "Etc/GMT-7",
-        "Etc/GMT-8",
-        "Etc/GMT-9",
-        "Etc/UTC",
-      ];
-      post.reply({
-        content: `@${post.username} The time zones you can use are: \n${
-          timeZoneList.join(", ")
-        } \nThis is not a full list as the full version is too long to send.`,
-      });
-      break;
+      try {
+        const timeZoneList = [
+          "Etc/GMT",
+          "Etc/GMT+1",
+          "Etc/GMT+10",
+          "Etc/GMT+11",
+          "Etc/GMT+12",
+          "Etc/GMT+2",
+          "Etc/GMT+3",
+          "Etc/GMT+4",
+          "Etc/GMT+5",
+          "Etc/GMT+6",
+          "Etc/GMT+7",
+          "Etc/GMT+8",
+          "Etc/GMT+9",
+          "Etc/GMT-1",
+          "Etc/GMT-10",
+          "Etc/GMT-11",
+          "Etc/GMT-12",
+          "Etc/GMT-13",
+          "Etc/GMT-14",
+          "Etc/GMT-2",
+          "Etc/GMT-3",
+          "Etc/GMT-4",
+          "Etc/GMT-5",
+          "Etc/GMT-6",
+          "Etc/GMT-7",
+          "Etc/GMT-8",
+          "Etc/GMT-9",
+          "Etc/UTC",
+        ];
+        post.reply({
+          content: `@${post.username} The time zones you can use are: \n${timeZoneList.join(", ")
+            } \nThis is not a full list as the full version is too long to send.`,
+        });
+      } catch (err) {
+        post.reply({
+          reply_to: [post.id],
+          content: errorMsg,
+        });
+      } finally {
+        break;
+      }
     }
+    case "time":
     case "gettime": {
-      const reqArgs = getParmas(post.content);
-      let zonedTime = datetime().toZonedTime("ETC/GMT");
-      let daySection = getDaySection(zonedTime.hour);
-      let replyContent: string = `@${post.username} ${
-        convertTo12H(zonedTime.hour)
-      }:${zonedTime.minute} ${daySection} (ETC/GMT)!`;
-      if (reqArgs?.length == 3) {
-        const timeZone = reqArgs[2];
-        if (masterTimeZoneList.includes(timeZone)) {
-          zonedTime = datetime().toZonedTime(timeZone);
-          daySection = getDaySection(zonedTime.hour);
-          replyContent = `@${post.username} ${
-            convertTo12H(zonedTime.hour)
-          }:${zonedTime.minute} ${daySection} (${timeZone})!`;
-        } else {
-          replyContent =
-            `@${post.username} I don't feel like telling you the time atm. \nERROR 35: Invalid Time Zone!`;
+      try {
+        let zonedTime = datetime().toZonedTime("ETC/GMT");
+        let daySection = getDaySection(zonedTime.hour);
+        let replyContent: string = `@${post.username} ${convertTo12H(zonedTime.hour)
+          }:${zonedTime.minute} ${daySection} (ETC/GMT)!`;
+        if (command?.length == 3) {
+          const timeZone = command[2];
+          if (masterTimeZoneList.includes(timeZone)) {
+            zonedTime = datetime().toZonedTime(timeZone);
+            daySection = getDaySection(zonedTime.hour);
+            replyContent = `@${post.username} ${convertTo12H(zonedTime.hour)
+              }:${zonedTime.minute} ${daySection} (${timeZone})!`;
+          } else {
+            replyContent =
+              `@${post.username} I don't feel like telling you the time atm. \nERROR 35: Invalid Time Zone!`;
+          }
         }
+        post.reply({
+          reply_to: [post.id],
+          content: replyContent,
+        });
+      } catch (err) {
+        post.reply({
+          reply_to: [post.id],
+          content: errorMsg,
+        });
+      } finally {
+        break;
       }
-      post.reply({
-        reply_to: [post.id],
-        content: replyContent,
-      });
-      break;
     }
-    case "gettime24h": {
-      const reqArgs = getParmas(post.content);
-      let zonedTime = datetime().toZonedTime("ETC/GMT");
-      let replyContent: string =
-        `@${post.username} ${zonedTime.hour}:${zonedTime.minute} (ETC/GMT)!`;
-      if (reqArgs?.length == 3) {
-        const timeZone = reqArgs[2];
-        if (masterTimeZoneList.includes(timeZone)) {
-          zonedTime = datetime().toZonedTime(timeZone);
-          replyContent =
-            `@${post.username} ${zonedTime.hour}:${zonedTime.minute} (${timeZone})!`;
-        } else {
-          replyContent =
-            `@${post.username} I don't feel like telling you the time atm. \nERROR 35: Invalid Time Zone!`;
+    case "time24":
+    case "gettime24": {
+      try {
+        let zonedTime = datetime().toZonedTime("ETC/GMT");
+        let replyContent: string =
+          `@${post.username} ${zonedTime.hour}:${zonedTime.minute} (ETC/GMT)!`;
+        if (command?.length == 3) {
+          const timeZone = command[2];
+          if (masterTimeZoneList.includes(timeZone)) {
+            zonedTime = datetime().toZonedTime(timeZone);
+            replyContent =
+              `@${post.username} ${zonedTime.hour}:${zonedTime.minute} (${timeZone})!`;
+          } else {
+            replyContent =
+              `@${post.username} I don't feel like telling you the time atm. \nERROR 35: Invalid Time Zone!`;
+          }
         }
-      }
 
-      post.reply({
-        reply_to: [post.id],
-        content: replyContent,
-      });
-      break;
+        post.reply({
+          reply_to: [post.id],
+          content: replyContent,
+        });
+      } catch (err) {
+        post.reply({
+          reply_to: [post.id],
+          content: errorMsg,
+        });
+      } finally {
+        break;
+      }
     }
     case "search": {
-      masterTimeZoneList.filter((value: String, index: number, array: String[]) => {
-        if (array[index].toString().toLowerCase().includes(value)) {
-          return true;
-        }
-      })
+      try {
+        if (typeof (command[2]) != 'string') break;
+        const searchValue = command[2];
+        let results = masterTimeZoneList.filter((value) => {
+          return value.toLowerCase().includes(searchValue.toLowerCase());
+        });
+        if (results.length == 0) results.push("No timezones found")
+        let replyContent: string = `Search Results for ${searchValue}:\n${results.splice(0, 20).join(", ")} \nMax number of results is 20.`;
+        post.reply({
+          reply_to: [post.id],
+          content: replyContent,
+        });
+      } catch (err) {
+        post.reply({
+          reply_to: [post.id],
+          content: errorMsg,
+        });
+      } finally {
+        break;
+      }
     }
     default:
       post.reply({
