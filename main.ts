5c78df970e8a4eb8e4d2d64ed83bb2825c28a26e
Finley <161162126+Finley224@users.noreply.github.com>
2024-09-25 11:27:57 -0400

    Update offline msg for the tropical storm

--- main.ts
+++ main.ts
@@ -299,7 +299,7 @@ Deno.addSignalListener("SIGINT", async () => {
   console.log("Exit");
   await updateQuote(
     meower,
-    "Bot is offline ):\nrun by @Blahaj, if you have any issues, contact support@whiskers.chat",
+    "Bot is offline, Due to severe weather.\nrun by @Blahaj, if you have any issues, contact support@whiskers.chat",
   );
   Deno.exit();
 });
